fd11f06a9fe8396be1ca5c28203289873c5bdb27
Benard Kong <benardkong@hotmail.com>
2019-10-22 20:55:14 -0700

    Hook up SCSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css'
+import './scss/styles.scss'
 
 ReactDOM.render(<p>Hello World</p>, document.getElementById('root'))
